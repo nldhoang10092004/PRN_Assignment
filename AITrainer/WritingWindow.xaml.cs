21708626672a56eb8a16c5bffdbbf5c113a7115f
Tra <nguyenthithanhtra1808@gmail.com>
2025-11-16 12:16:22 +0700

    WPFApp

---
+++ AITrainer/WritingWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFAPP
+{
+    /// <summary>
+    /// Interaction logic for WritingWindow.xaml
+    /// </summary>
+    public partial class WritingWindow : Page
+    {
+        public WritingWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerateTopic_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Previous_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
